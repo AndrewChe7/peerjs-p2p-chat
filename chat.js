7069c397f4667fd017ecb9100429fd1f4b5268cc
AndrewChe7 <Chernjakov2@gmail.com>
2017-06-22 15:09:40 +0300

    fixes

---
+++ chat.js
@@ -0,0 +1,8 @@
+$(document).ready (function () {
+  $("#chat-window").append("<p>hello world!</p>");
+});
+
+$("#send-button").onclick (function () {
+  $("#chat-window").append("<p>hello world!</p>");
+
+});
